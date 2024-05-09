2b8bf9514e1c75fdf05363ba4ca9f3d52bd005b1
andrewkimm99 <andrewkimm99@gmail.com>
2024-05-08 23:30:03 -0500

    Add files via upload

--- pa5.py
+++ pa5.py
@@ -1,6 +1,12 @@
+'''
+hw5
+'''
 import math
 
 def gcd(a, b):
+    '''
+    do gcd using recursion
+    '''
     if b > a:
         temp = b
         b = a
@@ -10,35 +16,36 @@ def gcd(a, b):
     return gcd(b, a%b)
 
 def remove_pairs(s):
+    '''
+    remove pairs using recursion
+    '''
     uturn_l = ['SN', 'NS', 'WE', 'EW']
     if len(s) < 2:
         return s
-    if s[:2] in uturn_l:   
+    if s[:2] in uturn_l:
         return remove_pairs(s[2:])
-    else:
-        return s[0] + remove_pairs(s[1:])
+    return s[0] + remove_pairs(s[1:])
 
-def bisection_root(f, x1, x2):
-    if abs(f(x1)) <= 0.001:
-        return x1
-    elif abs(f(x2)) <= 0.001:
-        return x2
-    midpoint = (x1 + x2) / 2
+def bisection_root(f, x_1, x_2):
+    '''
+    find function root using bisection and recursion
+    '''
+    if abs(f(x_1)) <= 0.0000001:
+        return x_1
+    if abs(f(x_2)) <= 0.0000001:
+        return x_2
+    midpoint = (x_1 + x_2) / 2
     if f(midpoint) < 0:
-        if f(x2) > 0:
-            return bisection_root(f, midpoint, x2)
-        elif f(x1) > 0:
-            return bisection_root(f, midpoint, x1)
+        if f(x_2) > 0:
+            return bisection_root(f, midpoint, x_2)
+        elif f(x_1) > 0:
+            return bisection_root(f, midpoint, x_1)
         else:
             raise ValueError("Both x values have same sign")
     elif f(midpoint) > 0:
-        if f(x2) < 0:
-            return bisection_root(f, midpoint, x2)
-        elif f(x1) < 0:
-            return bisection_root(f, midpoint, x1)
+        if f(x_2) < 0:
+            return bisection_root(f, midpoint, x_2)
+        elif f(x_1) < 0:
+            return bisection_root(f, midpoint, x_1)
         else:
             raise ValueError("Both x values have same sign")
-
-
-
-
